6fae3972e3f3b26ae52a56bd2885915fef25cef3
Nguyen Huy Hung <hungnh103@gmail.com>
2022-04-17 11:15:49 +0700

    refactor

--- catalog/models.py
+++ catalog/models.py
@@ -1,8 +1,9 @@
+from datetime import date
+import uuid
+
 from django.db import models
 from django.urls import reverse
-import uuid
 from django.contrib.auth.models import User
-from datetime import date
 
 
 class Genre(models.Model):
